060be07f96f9ea7e1f705933dcffdc82382a9be1
Roxana Copocean <roxana.mica@gmail.com>
2023-01-27 15:42:22 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
+import Navbar from './components/Navbar';
 
 export default function App() {
 	return (
 		<div className="App">
+			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/login" element={<Login />} />

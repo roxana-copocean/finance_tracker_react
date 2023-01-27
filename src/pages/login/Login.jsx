0e2f15d404838123b4a774823451d6f527a20328
Roxana Copocean <roxana.mica@gmail.com>
2023-01-27 16:09:21 +0200

    login/signup page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,25 @@
 import styles from './Login.module.css';
+import { useState } from 'react';
 export default function Login() {
-	return <div />;
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		console.log(email, password);
+	};
+	return (
+		<form className={styles['login-form']} onSubmit={handleFormSubmit}>
+			<h2>Login</h2>
+			<label>
+				<span>email:</span>
+				<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+			</label>
+			<label>
+				<span>passwords:</span>
+				<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+			</label>
+			<button className="btn">Login</button>
+		</form>
+	);
 }
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,5 +1,32 @@
 import styles from './Signup.module.css';
+import { useState } from 'react';
 
 export default function Signup() {
-	return <div>Signup</div>;
+	const [ name, setName ] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+
+	const handleSignup = (e) => {
+		e.preventDefault();
+		console.log(name, email, password);
+	};
+	return (
+		<form className={styles['signup-form']} onSubmit={handleSignup}>
+			<h2>Signup</h2>
+			<label>
+				<span>name:</span>
+				<input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+			</label>
+
+			<label>
+				<span>email:</span>
+				<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+			</label>
+			<label>
+				<span>passwords:</span>
+				<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+			</label>
+			<button className="btn">Signup</button>
+		</form>
+	);
 }
